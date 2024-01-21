4e14397537e4e3a318f57bc02aa850e2642b22b6
TomerSivan <sivantomer10@gmail.com>
2024-01-21 21:16:46 +0200

    Constant Changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,7 +4,6 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -18,9 +17,6 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
--- src/main/java/frc/robot/Util/SwerveModule.java
+++ src/main/java/frc/robot/Util/SwerveModule.java
@@ -4,9 +4,16 @@
 
 package frc.robot.Util;
 
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.CANcoderConfigurator;
+import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.configs.MagnetSensorConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfigurator;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.AbsoluteSensorRangeValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.signals.SensorDirectionValue;
 
 /** Add your docs here. */
 public class SwerveModule {
@@ -14,8 +21,8 @@ public class SwerveModule {
     private TalonFX steerMotor; // Falcon 500 motor responsible for steering the module
     private CANcoder absEncoder; // Absolute encoder responsible for keeping track of module position
 
-    private final boolean absoluteEncoderReversed;
     private final double absoluteEncoderOffset;
+    private final boolean absoluteEncoderReversed;
 
     public SwerveModule(int driveMotorID, int steerMotorID, int absEncoderID, boolean absoluteEncoderReversed,
      boolean steerMotorReversed, boolean driveMotorReversed,double absoluteEncoderOffset) {
@@ -23,8 +30,8 @@ public class SwerveModule {
         this.steerMotor = new TalonFX(steerMotorID);
         this.absEncoder = new CANcoder(absEncoderID);
 
-        this.absoluteEncoderReversed = absoluteEncoderReversed;
         this.absoluteEncoderOffset = absoluteEncoderOffset;
+        this.absoluteEncoderReversed = absoluteEncoderReversed;
 
         this.driveMotor.setInverted(driveMotorReversed);
         this.steerMotor.setInverted(steerMotorReversed);
@@ -32,8 +39,38 @@ public class SwerveModule {
         this.driveMotor.setNeutralMode(NeutralModeValue.Brake);
         this.steerMotor.setNeutralMode(NeutralModeValue.Brake);
 
-        this.driveMotor.getVelocity();
-        
+        configMotors();
+        configEnc();
+    }
+    private void configEnc() {
+        CANcoderConfigurator canConfig = this.absEncoder.getConfigurator();
+        MagnetSensorConfigs magnetConfig = new MagnetSensorConfigs();
 
+        magnetConfig.MagnetOffset = this.absoluteEncoderOffset;
+        magnetConfig.SensorDirection = this.absoluteEncoderReversed?
+        SensorDirectionValue.CounterClockwise_Positive:
+        SensorDirectionValue.Clockwise_Positive;
+        magnetConfig.AbsoluteSensorRange = AbsoluteSensorRangeValue.Signed_PlusMinusHalf;
+
+        canConfig.apply(magnetConfig);
+    }
+    private void configMotors() {
+        TalonFXConfigurator driveConfig = this.driveMotor.getConfigurator();
+        TalonFXConfigurator steerConfig = this.steerMotor.getConfigurator();
+
+        FeedbackConfigs driveFeedbackConfigs = new FeedbackConfigs();
+        FeedbackConfigs steerFeedbackConfigs = new FeedbackConfigs();
+
+        driveFeedbackConfigs.RotorToSensorRatio = 1;
+        driveFeedbackConfigs.SensorToMechanismRatio = 1;
+
+        steerFeedbackConfigs.RotorToSensorRatio = 1;
+        steerFeedbackConfigs.SensorToMechanismRatio = 1;
+
+        driveConfig.apply(driveFeedbackConfigs);
+        steerConfig.apply(steerFeedbackConfigs);
+    }
+    public double getModuleAngle() {
+        return this.absEncoder.getPosition().getValue()*360;
     }
 }
