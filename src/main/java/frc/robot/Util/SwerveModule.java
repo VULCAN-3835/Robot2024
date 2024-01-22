338a5666316671087d3e92a6a361758e9de3c949
TomerSivan <sivantomer10@gmail.com>
2024-01-22 17:16:42 +0200

    SwerveModule Angle Control

--- src/main/java/frc/robot/Util/SwerveModule.java
+++ src/main/java/frc/robot/Util/SwerveModule.java
@@ -4,16 +4,29 @@
 
 package frc.robot.Util;
 
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.CANcoderConfiguration;
 import com.ctre.phoenix6.configs.CANcoderConfigurator;
 import com.ctre.phoenix6.configs.FeedbackConfigs;
 import com.ctre.phoenix6.configs.MagnetSensorConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.configs.TalonFXConfigurator;
+import com.ctre.phoenix6.controls.ControlRequest;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.VelocityTorqueCurrentFOC;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.AbsoluteSensorRangeValue;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.ctre.phoenix6.signals.SensorDirectionValue;
+
+import edu.wpi.first.hal.simulation.ConstBufferCallback;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import frc.robot.Constants;
 
 /** Add your docs here. */
 public class SwerveModule {
@@ -23,6 +36,19 @@ public class SwerveModule {
 
     private final double absoluteEncoderOffset;
     private final boolean absoluteEncoderReversed;
+
+    private SwerveModuleState state;
+
+    private StatusSignal<Double> m_drivePosition; // Drive position supplier
+    private StatusSignal<Double> m_driveVelocity; // Drive velocity supplier
+    private StatusSignal<Double> m_steerPosition; // Steer position supplier
+    private StatusSignal<Double> m_steerVelocity; // Steer velocity supplier
+
+    private BaseStatusSignal[] m_signals; // Array of the suppliers
+
+    private SwerveModulePosition swervePosition = new SwerveModulePosition();
+
+    private PositionVoltage angleSetter = new PositionVoltage(0);
 
     public SwerveModule(int driveMotorID, int steerMotorID, int absEncoderID, boolean absoluteEncoderReversed,
      boolean steerMotorReversed, boolean driveMotorReversed,double absoluteEncoderOffset) {
@@ -39,38 +65,84 @@ public class SwerveModule {
         this.driveMotor.setNeutralMode(NeutralModeValue.Brake);
         this.steerMotor.setNeutralMode(NeutralModeValue.Brake);
 
-        configMotors();
         configEnc();
-    }
-    private void configEnc() {
-        CANcoderConfigurator canConfig = this.absEncoder.getConfigurator();
-        MagnetSensorConfigs magnetConfig = new MagnetSensorConfigs();
+        configDriveMotor();
+        configSteerMotor(absEncoderID);
 
-        magnetConfig.MagnetOffset = this.absoluteEncoderOffset;
-        magnetConfig.SensorDirection = this.absoluteEncoderReversed?
+        m_drivePosition = driveMotor.getPosition();
+        m_driveVelocity = driveMotor.getVelocity();
+        m_steerPosition = absEncoder.getPosition();
+        m_steerVelocity = absEncoder.getVelocity();
+
+        m_signals = new BaseStatusSignal[4];
+        m_signals[0] = m_drivePosition;
+        m_signals[1] = m_driveVelocity;
+        m_signals[2] = m_steerPosition;
+        m_signals[3] = m_steerVelocity;
+
+    }
+
+    private void configEnc() { // TODO: Check all constants
+        CANcoderConfiguration canConfigs = new CANcoderConfiguration();
+
+        canConfigs.MagnetSensor.MagnetOffset = this.absoluteEncoderOffset;
+        canConfigs.MagnetSensor.SensorDirection = this.absoluteEncoderReversed?
         SensorDirectionValue.CounterClockwise_Positive:
         SensorDirectionValue.Clockwise_Positive;
-        magnetConfig.AbsoluteSensorRange = AbsoluteSensorRangeValue.Signed_PlusMinusHalf;
 
-        canConfig.apply(magnetConfig);
+        this.absEncoder.getConfigurator().apply(canConfigs);
     }
-    private void configMotors() {
-        TalonFXConfigurator driveConfig = this.driveMotor.getConfigurator();
-        TalonFXConfigurator steerConfig = this.steerMotor.getConfigurator();
 
-        FeedbackConfigs driveFeedbackConfigs = new FeedbackConfigs();
-        FeedbackConfigs steerFeedbackConfigs = new FeedbackConfigs();
+    private void configSteerMotor(int absEncoderID) { // TODO: Check all constants
+        TalonFXConfiguration steerConfigs = new TalonFXConfiguration();
 
-        driveFeedbackConfigs.RotorToSensorRatio = 1;
-        driveFeedbackConfigs.SensorToMechanismRatio = 1;
+        steerConfigs.Slot0 = Constants.ModuleConstants.getSteerMotorGains();
 
-        steerFeedbackConfigs.RotorToSensorRatio = 1;
-        steerFeedbackConfigs.SensorToMechanismRatio = 1;
+        steerConfigs.Feedback.FeedbackRemoteSensorID = absEncoderID;
+        steerConfigs.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.FusedCANcoder;
+        steerConfigs.Feedback.RotorToSensorRatio = Constants.ModuleConstants.kSteerMotorGearRatio;
 
-        driveConfig.apply(driveFeedbackConfigs);
-        steerConfig.apply(steerFeedbackConfigs);
+        steerConfigs.ClosedLoopGeneral.ContinuousWrap = true;
+
+        this.steerMotor.getConfigurator().apply(steerConfigs);
     }
-    public double getModuleAngle() {
-        return this.absEncoder.getPosition().getValue()*360;
+
+    private void configDriveMotor() { // TODO: Do drive motor configs
+        
+    }
+
+    public SwerveModulePosition getPosition(boolean refresh) {
+        if (refresh) {
+            m_drivePosition.refresh();
+            m_driveVelocity.refresh();
+            m_steerPosition.refresh();
+            m_steerVelocity.refresh();
+        }
+
+        double drive_rot =
+            BaseStatusSignal.getLatencyCompensatedValue(m_drivePosition, m_driveVelocity);
+        double angle_rot =
+            BaseStatusSignal.getLatencyCompensatedValue(m_steerPosition, m_steerVelocity);
+
+        swervePosition.distanceMeters = drive_rot; // TODO: Into meters
+        swervePosition.angle = Rotation2d.fromRotations(angle_rot);
+
+        return swervePosition;
+    }
+
+    public void set(SwerveModuleState state) {
+        this.state = state;
+
+        m_steerPosition.refresh();
+        m_steerVelocity.refresh();
+
+        double angle_rot =
+            BaseStatusSignal.getLatencyCompensatedValue(m_steerPosition, m_steerVelocity);
+
+        SwerveModuleState optimized = SwerveModuleState.optimize(this.state, Rotation2d.fromRotations(angle_rot));
+
+        double angleToSetDeg = optimized.angle.getRotations();
+        steerMotor.setControl(angleSetter.withPosition(angleToSetDeg));
+        
     }
 }
