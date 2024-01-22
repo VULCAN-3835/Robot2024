98325014a8563e37ff0e5cd37d833561b328ee75
Oreoarc <justeyal445@gmail.com>
2024-01-22 20:05:54 +0200

    Finished SetMotorMode method

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -19,8 +19,6 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -22,10 +22,8 @@ public class IntakeSubsystem extends SubsystemBase {
   private PIDController angleController;
   public enum state {
     STATE1,
-    STATE2,
-    STATE3
+    STATE2
   }
-
   public IntakeSubsystem() {
     intakeMotor = new Spark(Constants.IntakeSubsystemConstants.kIntakeMotorPort);
     angleMotor = new Spark(Constants.IntakeSubsystemConstants.kAngleMotorPort);
@@ -41,16 +39,13 @@ public class IntakeSubsystem extends SubsystemBase {
   public void setMotorMode(state state) { 
     switch (state) {
       case STATE1:
-        // sets motor mode as STATE1
+        intakeMotor.set(Constants.IntakeSubsystemConstants.kIntakeMotorIntakeSpeed); //Intake
         break;
       case STATE2:
-        // sets motor mode as STATE2
-        break;
-      case STATE3:
-        // sets motor mode as STATE3
+        intakeMotor.set(Constants.IntakeSubsystemConstants.kIntakeMotorOutputSpeed); //Output
         break;
       default:
-              intakeMotor.set(0);
+         intakeMotor.set(0);
         break;
     }
   }
@@ -75,3 +70,16 @@ public class IntakeSubsystem extends SubsystemBase {
     angleMotor.set(angleController.calculate(angleEncoder.getAbsolutePosition()));
   }
 }
+
+// public static class IntakeSubsystemConstants{
+//     public static final int kIntakeMotorPort = 10;
+//     public static final int kAngleMotorPort = 11;
+//     public static final int kAngleEncoderChannel = 0;
+//     public static final int kPieceDetectorAnalogInputPort = 0;
+//     public static final int kLimitSwitchPort = 1;
+//     public static final double pieceDetector_DetectionThreshold = 2.5; //TODO: Find actual value
+//     public static final double kAngleEncoderOffset = 0.2; //TODO: Find actual value
+//     public static final double kKp = 0.5; //TODO: Find actual value
+//     public static final double kIntakeMotorOutputSpeed = 0.75; //TODO: Find Actual Speed needed, and find out which way the intake motor is facing
+//     public static final double kIntakeMotorIntakeSpeed = 0.75;
+//   }
