4d662e59891e82d1a4e52b72c64460cf5b728dfa
Oreoarc <justeyal445@gmail.com>
2024-02-11 20:16:03 +0200

    Speed Monitoring

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -9,27 +9,33 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 import frc.robot.Constants.ShooterConstants;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.math.system.LinearSystem;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 
 public class ShooterSubsystem extends SubsystemBase {
   private final TalonFX shooterMotor;
   private DoubleSolenoid ampPiston;
+  private final PIDController speedPIDController;
+  private final SimpleMotorFeedforward speedFeedForward;
 
   public ShooterSubsystem() {
-    this.shooterMotor = new TalonFX(ShooterConstants.kShooterMotorPort);
-    this.ampPiston = new DoubleSolenoid(PneumaticsModuleType.REVPH,ShooterConstants.kPistonForwardChannelNumber,ShooterConstants.kPistonReverseChannelNumber);
+    this.shooterMotor = new TalonFX(Constants.ShooterConstants.kShooterMotorPort);
+    this.ampPiston = new DoubleSolenoid(PneumaticsModuleType.REVPH,Constants.ShooterConstants.kPistonForwardChannelNumber,Constants.ShooterConstants.kPistonReverseChannelNumber);
+    this.speedPIDController = new PIDController(Constants.ShooterConstants.kSpeedPIDkP, 0, 0);
+    this.speedFeedForward = new SimpleMotorFeedforward(Constants.ShooterConstants.kspeedFFkS, Constants.ShooterConstants.kspeedFFkV, Constants.ShooterConstants.kspeedFFkA);
   }
 
-  /**
-     * sets the motor to the speed of collect
-  */
+  //Sets the motor to the speed of collect
   public void collect()
   {
-    shooterMotor.set(ShooterConstants.kCollectPower);
+    shooterMotor.set(Constants.ShooterConstants.kCollectPower);
   }
-  /**
-     * Stops the motor
-  */
+  //Stops the motor
   public void stopMotor(){
     shooterMotor.set(0);
   }
@@ -38,10 +44,11 @@ public class ShooterSubsystem extends SubsystemBase {
      * sets the motor to its desired speed
      * @param speed  the desired speed
   */
-  public void setShooterSpeed(double speed) { 
-    shooterMotor.set(speed);
+  public void setShooterSpeed(double shooterSpeed) { 
+    speedPIDController.setSetpoint(shooterSpeed);
+    shooterMotor.setVoltage(speedPIDController.calculate(shooterMotor.getRotorVelocity().getValue()) + speedFeedForward.calculate(shooterSpeed));
+    //The calculation is the current velocity(Pid output) + what is needed in order to get to the setpoint(FF output)
   }
-
   /**
      * true opens the piston
      * false closes the piston
